366550718e995c8a9661149f13a2c84a2ac94090
lasekmz <lasekmz@dukes.jmu.edu>
2017-03-16 16:58:15 -0400

    Add files via upload

--- challenge/__init__.py
+++ challenge/__init__.py
@@ -1,1 +1,10 @@
 def new_phrase(words):
+	import re
+
+	#words = input()
+	#get rid of punt. and spacing using a regex (regular expression) function
+	re.sub('[^A-Za-z0-9]+', '', words)
+	#print(wordse)
+
+
+	
